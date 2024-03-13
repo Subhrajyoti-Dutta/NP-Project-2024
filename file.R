acd159f1f56b77563611b3fa20791d2f927f9cd0
skanda185 <skanda.svc@gmail.com>
2024-03-14 01:38:48 +0530

    Distribution-Free Attempt 1

--- file.R
+++ file.R
@@ -1,2 +1,13 @@
-print("hello world")
-print("yoooo")
+library("fMultivar")
+
+k = 10000
+n = 20
+gen_dist = function(dist1, dist2, n){
+  x = dist1(n)
+  y = dist2(n)
+  cbind(x,y)
+}
+scorr = function(arr) {cor(arr[,1], arr[,2], method = "spearman")}
+dist1 <- replicate(k, gen_dist(rexp,rexp,n))
+iter <- apply(dist1, 3 , scorr)
+hist(iter)
