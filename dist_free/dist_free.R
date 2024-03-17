951ac13ab98685d36523e4a629b21affb07842f9
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2024-03-17 23:17:50 +0530

    add dist img

--- dist_free/dist_free.R
+++ dist_free/dist_free.R
@@ -1,16 +1,17 @@
 library("fMultivar")
 
-set.seed(3.14)
+# set.seed(3.14)
 
 k = 10000
-n = 12
+n = 50
 gen_dist = function(){
   x = rlogis(n)     #dist1
   y = rlogis(n)     #dist2
   cbind(x,y)
 }
-scorr = function(arr) {cor(arr[,1], arr[,2], method = "spearman")}
+scorr = function(arr) {sqrt(n-1)*cor(arr[,1], arr[,2], method = "spearman")}
 dist1 <- replicate(k, gen_dist())
 rho <- apply(dist1, 3 , scorr)
 hist(rho,xlim = c(-1,1),main = "Logistic Distribution",xlab="Correlation (rho)")
 
+plot(ecdf(rho),xlim=c(-4,4))
--- var_with_n/var_with_n.R
+++ var_with_n/var_with_n.R
@@ -13,7 +13,10 @@ for (n in nvals){
   scorr = function(arr) {cor(arr[,1], arr[,2], method = "spearman")}
   dist1 <- replicate(k, gen_dist())
   rho <- apply(dist1, 3 , scorr)
-  png(file = paste("dist_n=",n,".png",sep=""))
-  hist(rho,xlim = c(-1,1),main = "Normal Distribution",xlab="Correlation (rho)")
+  png(file = paste("dist_n=",n,".png",sep=""), width = 960, height = 480)
+  par(mfrow=c(1,2))
+  hist(rho,xlim = c(-3,3),main = "",xlab="Correlation (rho)")
+  plot(ecdf(rho),xlim=c(-3,3),main = "",xlab="Correlation (rho)")
+  mtext(paste("Distribution at n =",n), side = 3, line = - 2, outer = TRUE)
   dev.off()
 }
