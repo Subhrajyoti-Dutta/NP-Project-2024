d8570556bc85f9d87958c6194869a851f73040fa
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2024-03-20 02:38:57 +0530

    some thing some thing

--- power/power.R
+++ power/power.R
@@ -10,54 +10,65 @@ scorr = function(arr) {
 # by simulating null distribution of rho for small n, 
 # and using asymptotic normality for large n
 
-cut_off = function(n, alpha, alt) {
- 
-  if (alt =='upper')
-    prob = 1 - alpha
-  
-  else if (alt =='lower')
-    prob = alpha
-  
-  else
-    prob = 1 - (alpha/2)    
+cut_off = function(n, prob, alt) {
+  # 
+  # if (alt =='upper')
+  #   prob = 1 - alpha
+  # 
+  # else if (alt =='lower')
+  #   prob = alpha
+  # 
+  # else
+  #   prob = 1 - (alpha/2)    
     # assuming null distribution of rho is symmetric about it's mean 0 (check)
     # make this remark when presenting the histograms showing variation with n
  
   k = 10000
   
-  if(n <=100) {
+  if(n <=10) {
     rho = replicate(k, scorr(rbinorm(n)))
     # rho = apply(data, 3, scorr)
+    # print(mean(rho))
     cp = quantile(rho, prob)
+    # hist(rho)
+    # print((ecdf(rho)(0.8)))
+    # plot(ecdf(rho))
   }
   
   else 
     cp = qnorm(prob, sd = 1/sqrt(n-1))
   
   names(cp) = NULL
-  
+  # print(cp)
   return(cp)
 }
 
 power = function(n, alpha, alt, func, delta) {
   
-  cp = cut_off(n, alpha, alt)
+  
   
   k = 10000
+  print("hell")
   rho = replicate(k, scorr(func(n, delta)))
   # rho = apply(data, 3, scorr)
-  
+  # print(c(mean(rho),var(rho)))
+  # print()
+  print("hello")
   if(alt == "upper") {
-    power = sum(rho >= cp)/k
+    cp = cut_off(n, 1-alpha, alt)
+    power = mean(rho > cp)
   }
   
   else if(alt == "lower") {
-    power = sum(rho <= cp)/k
+    cp = cut_off(n, alpha, alt)
+    power = mean(rho < cp)
   }
   
   else {
-    power = sum((rho >= cp) | (rho <= cp))/k
+    cp = cut_off(n, 1-alpha/2, alt)
+    power = sum((rho > cp) | (rho < cp))/k
   }
+  print("helllloooo")
   
   return(power)
 }
@@ -73,4 +84,5 @@ power = function(n, alpha, alt, func, delta) {
 nvals = c(2:10,seq(12,20,2),seq(30,100,10))
 
 
-sapply(nvals,function(n) power(n, 0.05, "upper", rbifgmcop, 0.5))
+powers = sapply(nvals,function(n) power(n, 0.05, "upper", rbifgmcop, 0.5))
+plot(nvals,powers)
