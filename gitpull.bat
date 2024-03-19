18bfc8f3768557231a9220e7a768a98ce815630e
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2024-03-19 15:45:59 +0530

    19-03-2024 15:45:59.33

--- gitpull.bat
+++ gitpull.bat
@@ -1,5 +1,5 @@
 git pull
 git add .
-git config user.name | set name
+for /f %%i in ('git config user.name') do set VAR=%%i
 echo %name%
-git commit -m "`git config user.name` %DATE% %TIME%"
+git commit -m " %DATE% %TIME%"
