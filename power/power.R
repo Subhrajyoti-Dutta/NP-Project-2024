2ddf62e651df8587964a5910314e8e72d9239b33
skanda185 <skanda.svc@gmail.com>
2024-03-22 16:14:34 +0530

    blah blah

--- power/power.R
+++ power/power.R
@@ -1,60 +1,36 @@
 # install.packages("VGAM")
+# install.packages("fMultivar")
 library("VGAM")
 library("fMultivar")
+
+k = 10000
 
 # computing spearman's correlation coefficient over an array of random number pairs
 scorr = function(arr) {
   cor(arr[,1], arr[,2], method = 'spearman')
-  }
+}
 
 # obtaining cut-off points for rho 
 # by simulating null distribution of rho for small n, 
 # and using asymptotic normality for large n
 
 cut_off = function(n, prob, alt) {
-  # 
-  # if (alt =='upper')
-  #   prob = 1 - alpha
-  # 
-  # else if (alt =='lower')
-  #   prob = alpha
-  # 
-  # else
-  #   prob = 1 - (alpha/2)    
-    # assuming null distribution of rho is symmetric about it's mean 0 (check)
-    # make this remark when presenting the histograms showing variation with n
- 
-  k = 10000
-  
+
   if(n <=10) {
     rho = replicate(k, scorr(rbinorm(n)))
-    # rho = apply(data, 3, scorr)
-    # print(mean(rho))
     cp = quantile(rho, prob)
-    # hist(rho)
-    # print((ecdf(rho)(0.8)))
-    # plot(ecdf(rho))
   }
   
   else 
     cp = qnorm(prob, sd = 1/sqrt(n-1))
   
   names(cp) = NULL
-  # print(cp)
   return(cp)
 }
 
 power = function(n, alpha, alt, func, delta) {
-  
-  
-  
-  k = 10000
-  print("hell")
   rho = replicate(k, scorr(func(n, delta)))
-  # rho = apply(data, 3, scorr)
-  # print(c(mean(rho),var(rho)))
-  # print()
-  print("hello")
+  
   if(alt == "upper") {
     cp = cut_off(n, 1-alpha, alt)
     power = mean(rho > cp)
@@ -69,22 +45,29 @@ power = function(n, alpha, alt, func, delta) {
     cp = cut_off(n, 1-alpha/2, alt)
     power = sum((rho > cp) | (rho < cp))/k
   }
-  print("helllloooo")
   
   return(power)
 }
 
+# Level of significance fixed as 0.05
+alpha = 0.05
 
-# Set-Ups
-# 
-# rbifgmcop() - uniform; -1/3 to 1/3 srho
-# rbiamhcop() - uniform; -0.271 to 0.478 rho
-# rbilogis() 
-# rbinorm() - normal; -1 to 1 rho
-# rbiclaytoncop() - uniform; 
-# x_bar & S
-nvals = c(2:10,seq(12,20,2),seq(30,100,10))
+# Power as a function of n
+
+nvals = c(2:10,seq(12,20,2),seq(30,100,10),seq(200,1000,100))
+powers = lapply(nvals, function(n) power(n, alpha, "upper", rbifgmcop, 0.5))#,mc.cores=4)
+plot(nvals,powers,log="x")
 
 
-powers = sapply(nvals,function(n) power(n, 0.05, "upper", rbifgmcop, 0.5))
-plot(nvals,powers)
+# Power as a function of association parameter (Fix n)
+n = 6
+
+delta_upper = c(seq(0.01,0.5,0.01))
+delta_lower = c(seq(-0.5,-0.01,0.01))
+delta_two.tailed = c(delta_lower, delta_upper)
+
+powers = sapply(delta_upper, function(delta) power(n, alpha, "upper", rnorm2d, delta))
+plot(delta_upper, powers, type = 'b', pch = 16, col = 'red', xlab = "Association Parameter", )
+ 
+powers = sapply(delta_lower, function(delta) power(n, alpha, "lower", rnorm2d, delta))
+plot(delta_lower,powers)
